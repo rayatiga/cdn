fe9f4e8dad0e62e6a9c6518f5be8bf3e26bed2c1
sultan <svnteentthones@gmail.com>
2023-08-28 09:24:26 +0700

    change navbar menu

--- public/js/rayatiga-cloud-template.js
+++ public/js/rayatiga-cloud-template.js
@@ -12,10 +12,9 @@ if (nav) {
                       <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul class="navbar-nav">
                           <li class="nav-item"><a class="nav-link${path == '/' ? (state = active) : (state = '')}"${path == '/' ? (state = aria) : (state = '')} href="/">Home</a></li>
-                          <li class="nav-item"><a class="nav-link${path == '/about.html' ? (state = active) : (state = '')}"${path == '/about.html' ? (state = aria) : (state = '')} href="/about.html">About</a></li>
-                          <li class="nav-item"><a class="nav-link${path == '/pricing.html' ? (state = active) : (state = '')}"${path == '/pricing.html' ? (state = aria) : (state = '')} href="/pricing.html">Pricing</a></li>
-                          <li class="nav-item d-lg-none"><a class="nav-link${path == '/contact.html' ? (state = active) : (state = '')}"${path == '/contact.html' ? (state = aria) : (state = '')} href="/contact.html">Contact</a></li>
-                          <li class="nav-item d-none d-lg-block"><a class="btn btn-dark border ms-2${path == '/contact.html' ? (state = active) : (state = '')}"${path == '/contact.html' ? (state = aria) : (state = '')} href="/contact.html">Contact</a></li>
+                          <li class="nav-item"><a class="nav-link${path == '/about' ? (state = active) : (state = '')}"${path == '/about' ? (state = aria) : (state = '')} href="/about.html">About</a></li>
+                          <li class="nav-item"><a class="nav-link${path == '/pricing' ? (state = active) : (state = '')}"${path == '/pricing' ? (state = aria) : (state = '')} href="/pricing.html">Pricing</a></li>
+                          <li class="nav-item"><a class="nav-link${path == '/contact' ? (state = active) : (state = '')}"${path == '/contact' ? (state = aria) : (state = '')} href="/contact.html">Contact</a></li>
                         </ul>
                       </div>
                     </div>
